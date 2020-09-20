76cd15878df0c2e9e4e779fd69d6b13592baa85f
labradorescence <orangenewyorkcreativeagency@gmail.com>
2020-09-19 20:56:47 -0400

    1234

--- r.js
+++ r.js
@@ -1,24 +1,42 @@
+function getDigit(num, place){
+    return Math.floor(Math.abs(num) / Math.pow(10, place) % 10)
+}
+
+
 function radixSort(numArr){
-    let maxDigits = 0;
-    for(let i = 0; i < numArr.length; i++){
-        maxDigits = Math.max(maxDigits, digitCount(nums[i]))
-    }
 
     const digitCount = (num) => {
         if(num === 0) return 1;
         return Math.floor(Math.log10(Math.abs(num))) +1
     }
+
+   
+    let maxDigits = 0;
     
-    for(let k = 0; i < maxDigits; k++){
+    for(let i = 0; i < numArr.length; i++){
+        maxDigits = Math.max(maxDigits, digitCount(numArr[i]))
+    }
+    console.log(maxDigits)
+
+
+
+    for(let k = 0; k < maxDigits; k++){
         let digitBuckets = Array.from({length: 10}, () => [])
         console.log("digitBucket1", digitBuckets)
 
+        
         for(let j = 0; j < numArr.length; j++){
-            
-
-
+            let digit = getDigit(numArr[j], k)   
+               console.log(digit)
+               digitBuckets[digit].push(numArr[j])
         }
+
+        numArr = [].concat(...digitBuckets)
+
     }
+
+    return numArr
 
 }
 
+console.log(radixSort([23,334,21, 3421, 1453, 9898, 7323]))
